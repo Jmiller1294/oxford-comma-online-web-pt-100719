0f91ce5c9b63d8eeec14386f45c15e9bec2fbdd1
Justin Miller <jayman1294@gmail.com>
2019-10-13 08:30:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,19 +1,14 @@
 def oxford_comma(array)
+
 new_array = []
 
-if array.length == 1 
-  array.join
-elsif array.length == 2
-array.join(" and ")
-else
-  
-  
-  
-  
-  new_array = "and " + array.pop
-  array << new_array
-  
-  array.join(", ")
-end
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else
+    new_array = "and " + array.pop
+    array << new_array
+    array.join(", ")
+  end
 end
