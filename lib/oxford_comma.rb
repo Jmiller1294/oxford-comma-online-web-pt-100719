0fce4cdb657972a5485861ff0b75226c54fe4e45
Justin Miller <jayman1294@gmail.com>
2019-10-13 07:51:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,13 @@
 def oxford_comma(array)
+new_array = []
+
 if array.length == 1 
   array.join
 elsif array.length == 2
 array.join(" and ")
 else
   array.join(", ")
-  array.unshift
+  
 
 end
 
